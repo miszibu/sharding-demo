83e694968d553eb7411c9730beff1f1ed54f59c3
zibu <varzibu@163.com>
2018-12-11 11:35:41 +0800

    [Add] init commit

---
+++ src/main/java/com/zibu/shardingdemo/ShardingDemoApplication.java
@@ -0,0 +1,12 @@
+package com.zibu.shardingdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShardingDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ShardingDemoApplication.class, args);
+    }
+}
---
+++ src/test/java/com/zibu/shardingdemo/ShardingDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zibu.shardingdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShardingDemoApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
